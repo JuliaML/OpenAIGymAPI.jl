5a57f1fcba6f1ff11c291577969a5406f943fc62
paulhendricks <paul.hendricks.2013@owu.edu>
2017-03-06 21:27:42 -0500

    added unit test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,27 @@
 using OpenAIGymAPI
 using Base.Test
 
-# write your own tests here
-@test 2 == 2
+# define helper functions
+function get_remote_base(host = "http://127.0.0.1", port = "5000")
+  return(host * ":" * port)
+end
+
+# set server up
+# to be edited
+
+# tear server down
+# to be edited
+
+########## TESTS ##########
+
+##### Valid use cases #####
+function test_create_destroy()
+  client = GymClient(get_remote_base())
+  instance_id = env_create(client, "CartPole-v0")
+  @test haskey(env_list_all(client), instance_id)
+  env_close(client, instance_id)
+  @test !haskey(env_list_all(client), instance_id)
+  return(nothing)
+end
+
+test_create_destroy()
